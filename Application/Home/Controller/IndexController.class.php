c8ffef4e5e1d7c6b9e90319f61082fb8c0ccbb92
xiongchx <loveryuchi@vip.qq.com>
2019-03-04 20:23:27 +0800

    推荐商品展示

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -6,7 +6,15 @@ class IndexController extends CommonController {
 
     public function index() {
         // 获得分类列表
-        $data['cate']=D('category')->getList();
+        $data['cate'] = D('category')->getList();
+        // 获得推荐商品
+        $data['best'] = M('goods')->field(
+                'gid,gname,price,thumb'  // 取 id 名字 价格 图片
+                )->where(array(
+                    'is_best'=>'yes',  // 是推荐商品
+                    'status'=>'yes',   // 已上架
+                    'recycle'=>'no'    // 不在回收站
+                    ))->limit(5)->select();
         $this->assign($data);
         $this->display();
     }
